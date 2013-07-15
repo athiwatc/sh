baa8f8b5d32eda6c06f15486e583d7e0ca5cfc9d
Athiwat Chunlakhan <athiwatc@gmail.com>
2013-07-15 14:46:39 +0200

    Fix router.js formatting

--- client/router.js
+++ client/router.js
@@ -1,13 +1,13 @@
 // ##Router
 
-//Add the router path
+// Add the router path
 Meteor.Router.add({
-	//Home root path, display the home page.
+	// Home root path, display the home page.
     '/': 'home',
-    //Else just go to the path with the same template names.
+    // Else just go to the path with the same template names.
     '/:go': function(go) {
         return go;
     },
-    //Other directory will be given a not found status.
+    // Other directory will be given a not found status.
     "*": "not_found"
 });
